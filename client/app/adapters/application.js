9748697ef2ae4607d2aa178befc7858f710a436a
John K. Paul <john@johnkpaul.com>
2018-02-05 15:36:56 -0500

    Add bootstrap and rest adapter

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
-export default DS.JSONAPIAdapter.extend({
-  namespace: 'api'
+export default DS.RESTAdapter.extend({
+  host: 'http://localhost:3000', 
+  namespace: 'api/v1'
 });
