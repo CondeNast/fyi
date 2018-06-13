b4b6f04cf735e1b6ff4eaaee5cd2517a74bc912e
Gautam Arora <gautamsarora@gmail.com>
2018-06-13 13:18:39 -0400

    + repo created events

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 module.exports = robot => {
-  // Your code here
   robot.log('Yay, the robot was loaded!')
 
-  robot.on('label.created', async context => {
-    robot.log('Yay, my robot is loaded')
-    context.log(context)
+  robot.on('repository.created', async context => {
+    context.log(`new repo: ${context.payload.repository.name} created by ${context.payload.sender.login}`)
   });
-
-  // For more information on building robots:
-  // https://probot.github.io/docs/
-
-  // To get your robot running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
