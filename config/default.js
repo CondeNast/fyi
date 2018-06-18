ffeb132ee8d655ead48b25d924ec26be0f1255d1
John K. Paul <john@johnkpaul.com>
2018-06-18 14:37:19 -0400

    Add Node Config

---
+++ config/default.js
@@ -0,0 +1,11 @@
+let username = require("os").userInfo().username
+
+module.exports = {
+  "database": {
+    "username": username,
+    "password": null,
+    "database": "easy-fyi-development",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  }
+}
