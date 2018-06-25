b739cdf209470da02da70b523ae7d62a75de6230
John K. Paul <john@johnkpaul.com>
2018-06-25 13:28:44 -0400

    Update vault path for development credentials

--- scripts/github.private.key.vault.js
+++ scripts/github.private.key.vault.js
@@ -1,6 +1,14 @@
 const CNVault = require('@condenast/cn-vault')
+let EASY_FYI_SECRET_PATH
 
-const EASY_FYI_SECRET_PATH = 'secret/architecture/easy-fyi'
+const env = process.env.NODE_ENV || "development"
+
+if (env === "production") {
+  EASY_FYI_SECRET_PATH = `secret/architecture/easy-fyi/production`
+}
+else {
+  EASY_FYI_SECRET_PATH = `secret/architecture/easy-fyi/nonprod/${env}`
+}
 
 module.exports = () => {
   CNVault.getInstance().getSecrets([EASY_FYI_SECRET_PATH])
