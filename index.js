2e873f44972e091de370933c63b962c39df7ec69
John K. Paul <john@johnkpaul.com>
2018-07-06 14:29:30 -0400

    Pipe stdout and stderr for departures

--- index.js
+++ index.js
@@ -2,19 +2,14 @@ if (require.main === module) {
   const { spawn } = require('child_process')
   let start = spawn('npm', ['start'], {env: {...process.env, PORT: process.env.NODE_PORT || 8081}})
 
-  start.stdout.on('data', (data) => {
-    process.stdout.write(`stdout: ${data}`)
-  })
+  start.stdout.pipe(process.stdout)
 
-  start.stderr.on('data', (data) => {
-    process.stderr.write(`stderr: ${data}`)
-  })
+  start.stderr.pipe(process.stderr)
 
   start.on('close', (code) => {
     process.stdout.write(`child process exited with code ${code}`)
   })
 }
-
 const metadata = require('probot-metadata')
 const commands = require('probot-commands')
 const filter = require('./middleware/filter')
