bd97fb1765297c68bef21767340a0c4d07fb466a
John K. Paul <john@johnkpaul.com>
2018-07-11 16:47:44 -0400

    Fix staging deployment

---
+++ robot.js
@@ -0,0 +1,38 @@
+const commands = require('probot-commands')
+
+const repoCreated = require('./handlers/github/events/repoCreated')
+const repoIdentified = require('./handlers/http/repoIdentified')
+const fyiRequested = require('./handlers/github/commands/fyiRequested')
+const fyiSkipped = require('./handlers/github/commands/fyiSkipped')
+const fyiSubmitted = require('./handlers/github/events/fyiSubmitted')
+const fyiAccepted = require('./handlers/github/commands/fyiAccepted')
+const fyiRejected = require('./handlers/github/commands/fyiRejected')
+const fyiReminder = require('./handlers/github/commands/fyiReminder')
+const fyiClosed = require('./handlers/github/commands/fyiClosed')
+const help = require('./handlers/github/commands/help')
+const digest = require('./handlers/digest')
+
+module.exports = robot => {
+  robot.log('🤖  Arch Bot is listening...')
+  robot.router.use(require('@condenast/express-dogstatsd')({}))
+
+  // github events
+  robot.on('repository.created', (context) => repoCreated(context, robot))
+  robot.on('issues.closed', (context) => fyiSubmitted(context, robot))
+
+  // http api
+  robot.router.post('/repos', repoIdentified(robot))
+
+  // pages
+  robot.router.get('/digest*', digest)
+
+  // github commands
+  commands(robot, 'request', async (context, command) => fyiRequested(context, command, robot))
+  commands(robot, 'skip', async (context, command) => fyiSkipped(context, command, robot))
+  commands(robot, 'accept', async (context, command) => fyiAccepted(context, command, robot))
+  commands(robot, 'reject', async (context, command) => fyiRejected(context, command, robot))
+  commands(robot, 'close', async (context, command) => fyiClosed(context, command, robot))
+  commands(robot, 'remind', async (context, command) => fyiReminder(context, command, robot))
+  commands(robot, 'help', async (context, command) => help(context, command, robot))
+}
+
