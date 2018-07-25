1b9532ada4bade6ee1c7688ef824e2abaebc827a
Gautam Arora <gautamsarora@gmail.com>
2018-07-25 17:12:50 -0400

    + post to slack

--- src/handlers/http/fyiAutoReminder.js
+++ src/handlers/http/fyiAutoReminder.js
@@ -1,8 +1,11 @@
 const metadata = require('probot-metadata')
 const moment = require('moment')
+const slack = require('../../services/slack')
+const logPrefix = require('../../utils/logPrefix')
 const configGH = require('config').github
 const authGH = require('../../services/github')
 const fyiReminderHandler = require('../github/commands/fyiReminder')
+const Fyi = require('../../models').Fyi
 
 module.exports = (app) => {
   return (request, response) => {
@@ -16,11 +19,14 @@ module.exports = (app) => {
       dataChunks.push(chunk)
     })
     request.on('end', async () => {
+      let { adminOrg, adminRepo } = configGH
+      const LOG_PREFIX_ADMIN = logPrefix('fyiAutoReminder', adminOrg, adminRepo)
+      app.log(`${LOG_PREFIX_ADMIN} http request recieved`)
+
       // fetch all open issues in fyis repo tagged with 'fyi-requested'
       // for each, check if request issue exists and its date
       // if over 1 week, post a reminder comment in request issue
       // (optional) post in fyi issue that a reminder has been posted
-      let { adminOrg, adminRepo } = configGH
       let github = await authGH({app, org: adminOrg})
       const { data: openRequestIssues } = await github.issues.getForRepo({
         owner: adminOrg,
@@ -28,9 +34,9 @@ module.exports = (app) => {
         state: 'open',
         labels: 'fyi-requested'
       })
-      for (const issue of openRequestIssues) {
-        const { org, repo, repoIssue } = await metadata({payload: {installation: {id: ''}}}, issue).get() || {}
-        console.log(org, repo, repoIssue)
+      app.log(`${LOG_PREFIX_ADMIN} open issues fetched`)
+      for (const adminIssue of openRequestIssues) {
+        const { org, repo, repoIssue, fyiName } = await metadata({payload: {installation: {id: ''}}}, adminIssue).get() || {}
         if (!org || !repo || !repoIssue) return
         let { data: { created_at: repoIssueCreatedAt } } = await github.issues.get({
           owner: org,
@@ -42,8 +48,9 @@ module.exports = (app) => {
         let repoIssueCreatedDaysAgo = now.diff(repoIssueCreatedAt, 'days')
         let repoIssueCreatedWeeksAgo = Math.floor(repoIssueCreatedDaysAgo / 7)
         let isReminderDay = repoIssueCreatedDaysAgo % 7 === 0
-        console.log(org, repo, repoIssueCreatedDaysAgo, isReminderDay, repoIssueCreatedWeeksAgo)
+
         if (isReminderDay && repoIssueCreatedWeeksAgo > 0) {
+          app.log(`${LOG_PREFIX_ADMIN} preparing to send reminder for ${org}/${repo}`)
           let message
           if (repoIssueCreatedWeeksAgo === 1) {
             message = '1-Week Reminder: Complete the requested FYI'
@@ -52,9 +59,8 @@ module.exports = (app) => {
           } else if (repoIssueCreatedWeeksAgo >= 2) {
             message = 'Final Reminder: Complete the requested FYI'
           }
-          console.log('sending reminder for', org, repo, repoIssueCreatedDaysAgo, isReminderDay, repoIssueCreatedWeeksAgo)
           const context = require('./fixtures/issue-comment-created.json')
-          context.payload.issue = issue
+          context.payload.issue = adminIssue
           context.payload.repository.name = adminRepo
           context.payload.organization.login = adminOrg
           context.payload.sender.login = 'API'
@@ -62,16 +68,22 @@ module.exports = (app) => {
           context.payload.source = 'API'
 
           context.issue = (issueData) => {
-            let start = {}
-            return Object.assign(start, issueData)
+            return Object.assign({}, issueData)
           }
           context.github = github
           context.log = app.log
 
           await fyiReminderHandler(context, app)
+          app.log(`${LOG_PREFIX_ADMIN} reminder sent for ${org}/${repo}`)
+
+          app.log(`${LOG_PREFIX_ADMIN} loading fyi model for ${fyiName} ...`)
+          let fyi = await Fyi.forName(fyiName)
+          app.log(`${LOG_PREFIX_ADMIN} fyi model loaded`)
+
+          await slack.post({type: 'fyi-autoreminder', context, org, repo, adminOrg, adminRepo, adminIssue: adminIssue.number, fyi})
+          app.log(`${LOG_PREFIX_ADMIN} slack message posted`)
         }
       }
-      console.log('Done')
       response.send({success: true})
     })
   }
