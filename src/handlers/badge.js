7d7b03bd58b97a44ba0f2d60e8f4fbeb064b6e5d
John K. Paul <john@johnkpaul.com>
2018-08-13 16:14:37 -0400

    Update badge/link to use ID _and_ fyiName

--- src/handlers/badge.js
+++ src/handlers/badge.js
@@ -1,5 +1,6 @@
 const confluence = require('../services/confluence')
 const badge = require('gh-badges')
+const Fyi = require('../models').Fyi
 const format = {
   text: ['fyi', 'passed'],
   colorscheme: 'green',
@@ -9,6 +10,12 @@ module.exports = async (request, response) => {
   if (!request.params.name) return
 
   let fyiName = request.params.name
+
+  if( fyiNameIsReallyAnID(fyiName) ){
+   let fyi = await Fyi.findById(fyiName)
+   fyiName = fyi.name
+  }
+
   const isFyiWritten = await confluence.isFyiWritten(fyiName)
   format.text[0] = `FYI for ${fyiName}`
   format.text[1] = isFyiWritten ? 'passed' : 'failed'
@@ -17,4 +24,7 @@ module.exports = async (request, response) => {
     response.setHeader('content-type', 'image/svg+xml')
     response.status(200).send(svg)
   })
+}
+function fyiNameIsReallyAnID(fyiName){
+  return !Number.isNaN(parseInt(fyiName))
 }
--- src/handlers/link.js
+++ src/handlers/link.js
@@ -1,8 +1,17 @@
 const confluence = require('../services/confluence')
+const Fyi = require('../models').Fyi
 module.exports = async (request, response) => {
   if (!request.params.name) return
 
   let fyiName = request.params.name
+  if( fyiNameIsReallyAnID(fyiName) ){
+   let fyi = await Fyi.findById(fyiName)
+   fyiName = fyi.name
+  }
   const fyiLink = await confluence.getFyiLink(fyiName)
   response.redirect(fyiLink)
+}
+
+function fyiNameIsReallyAnID(fyiName){
+  return !Number.isNaN(parseInt(fyiName))
 }
