4013d944e5964885a3bcba67e957188ac5395663
John K. Paul <john@johnkpaul.com>
2018-08-14 11:50:51 -0400

    Increase size of content column

---
+++ migrations/20180814143744-update_fyi_content_column_size.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.changeColumn('Fyis', 'content', {
+      type: Sequelize.STRING(32768)
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    queryInterface.changeColumn('Fyis', 'content', {
+      type: Sequelize.STRING(255)
+    
+    })
+  }
+};
