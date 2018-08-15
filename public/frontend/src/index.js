070a8846b97294193f3cf5ea93035ba954199209
John K. Paul <john@johnkpaul.com>
2018-08-15 16:14:25 -0400

    Add google analytics

--- public/frontend/src/index.js
+++ public/frontend/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { Router as BrowserRouter } from 'react-router-dom'
+import createHistory from 'history/createBrowserHistory'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-124050572-1');
+const history = createHistory()
+history.listen((location, action) => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+});
+
+ReactGA.set({ page: window.location.pathname });
+ReactGA.pageview(window.location.pathname);
+
+ReactDOM.render(<BrowserRouter history={history}><App /></BrowserRouter>, document.getElementById('root'))
 registerServiceWorker()
