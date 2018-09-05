ebf1cfe67896bc529f321f42473c899447b9b624
Gautam Arora <gautamsarora@gmail.com>
2018-09-05 16:19:14 -0400

    + update event structure for envs and az

--- src/handlers/deploys.js
+++ src/handlers/deploys.js
@@ -1,5 +1,6 @@
 const datadog = require('../services/datadog')
 const Fyi = require('../models').Fyi
+const moment = require('moment')
 
 module.exports = async (request, response) => {
 
@@ -11,17 +12,41 @@ module.exports = async (request, response) => {
     return response.send({error: "fyi not found"})
   }
 
-  let lastDeployEvents = await Promise.all(fyi.repos.map(async (path) => {
+  let latestDeployEvents = await Promise.all(fyi.repos.map(async (path) => {
     let [org, repo] = path.split('/')
     console.log(path, org, repo)
-    let events = await datadog({org, repo})
-    console.log(events.length)
-    if(events.length > 0) {
-      events[0].path = path
-      return events[0]
-    } else {
-      return {}
+    let { prodDeployEvents, nonprodDeployEvents } = await datadog({org, repo})
+
+    let deployEvent = {
+      prod: {
+        fyi: {
+          name: fyiName,
+          repo_path: path
+        },
+        datadog: {}
+      },
+      nonprod: {
+        fyi: {
+          name: fyiName,
+          repo_path: path
+        },
+        datadog: {}
+      }
     }
+    if(prodDeployEvents.length > 0) {
+      deployEvent.prod.fyi.date_happened_human = moment.unix(prodDeployEvents[0].date_happened).tz("America/New_York").format('MMMM Do YYYY, h:mm:ss a z')
+      let [azTag] = prodDeployEvents[0].tags.filter(t => t.startsWith("availability-zone:"))
+      deployEvent.prod.fyi.az = azTag.slice("availability-zone:".length)
+      deployEvent.prod.datadog = prodDeployEvents[0]
+    }
+
+    if(nonprodDeployEvents.length > 0) {
+      deployEvent.nonprod.fyi.date_happened_human = moment.unix(nonprodDeployEvents[0].date_happened).tz("America/New_York").format('MMMM Do YYYY, h:mm:ss a z')
+      let [azTag] = nonprodDeployEvents[0].tags.filter(t => t.startsWith("availability-zone:"))
+      deployEvent.nonprod.fyi.az = azTag.slice("availability-zone:".length)
+      deployEvent.nonprod.datadog = nonprodDeployEvents[0]
+    }
+    return deployEvent
   }))
-  response.send({events: lastDeployEvents})
+  response.send({events: latestDeployEvents})
 }
