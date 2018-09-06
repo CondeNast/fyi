768f47415fedd969f53c7738aba8227a72c7a44f
Gautam Arora <gautamsarora@gmail.com>
2018-09-06 15:59:05 -0400

    + break apart nonprod events as stag and ci

--- src/handlers/deploys.js
+++ src/handlers/deploys.js
@@ -14,7 +14,7 @@ module.exports = async (request, response) => {
 
   let latestDeployEvents = await Promise.all(fyi.repos.map(async (path) => {
     let [org, repo] = path.split('/')
-    let { prodDeployEvents, nonprodDeployEvents } = await datadog({org, repo})
+    let { prodDeployEvents, stagDeployEvents, ciDeployEvents } = await datadog({org, repo})
 
     let deployEvent = {
       prod: {
@@ -24,7 +24,14 @@ module.exports = async (request, response) => {
         },
         datadog: {}
       },
-      nonprod: {
+      stag: {
+        fyi: {
+          name: fyiName,
+          repo_path: path
+        },
+        datadog: {}
+      },
+      ci: {
         fyi: {
           name: fyiName,
           repo_path: path
@@ -39,11 +46,18 @@ module.exports = async (request, response) => {
       deployEvent.prod.datadog = prodDeployEvents[0]
     }
 
-    if(nonprodDeployEvents.length > 0) {
-      deployEvent.nonprod.fyi.date_happened_human = moment.unix(nonprodDeployEvents[0].date_happened).tz("America/New_York").format('MMMM Do YYYY, h:mm:ss a z')
-      let [azTag] = nonprodDeployEvents[0].tags.filter(t => t.startsWith("availability-zone:"))
-      deployEvent.nonprod.fyi.az = azTag.slice("availability-zone:".length)
-      deployEvent.nonprod.datadog = nonprodDeployEvents[0]
+    if(stagDeployEvents.length > 0) {
+      deployEvent.stag.fyi.date_happened_human = moment.unix(stagDeployEvents[0].date_happened).tz("America/New_York").format('MMMM Do YYYY, h:mm:ss a z')
+      let [azTag] = stagDeployEvents[0].tags.filter(t => t.startsWith("availability-zone:"))
+      deployEvent.stag.fyi.az = azTag.slice("availability-zone:".length)
+      deployEvent.stag.datadog = stagDeployEvents[0]
+    }
+
+    if(ciDeployEvents.length > 0) {
+      deployEvent.ci.fyi.date_happened_human = moment.unix(ciDeployEvents[0].date_happened).tz("America/New_York").format('MMMM Do YYYY, h:mm:ss a z')
+      let [azTag] = ciDeployEvents[0].tags.filter(t => t.startsWith("availability-zone:"))
+      deployEvent.ci.fyi.az = azTag.slice("availability-zone:".length)
+      deployEvent.ci.datadog = ciDeployEvents[0]
     }
     return deployEvent
   }))
