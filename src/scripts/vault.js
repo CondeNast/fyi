8acd02aa93ef94e30f0a1b756298d4bfa1e59f67
Gautam Arora <gautamsarora@gmail.com>
2018-09-14 16:09:18 -0400

    + auto renew vault token everyday

--- src/scripts/vault.js
+++ src/scripts/vault.js
@@ -4,6 +4,18 @@ const options = {
 }
 const vault = require('node-vault')(options)
 
+// auto-renew vault token
+let refreshRate = 24 * 60 * 60 * 1000
+setTimeout(function renewToken() {
+  vault.tokenRenewSelf()
+    .catch((err) => {
+      console.error(err)
+    })
+    .then(() => {
+      setTimeout(renewToken, refreshRate).unref();
+    });
+}, refreshRate).unref();
+
 const env = process.env.NODE_ENV || 'development'
 let EASY_FYI_SECRET_PATH
 
