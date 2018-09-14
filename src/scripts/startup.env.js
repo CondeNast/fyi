0d6d35b2ba87b528c13e923ac086eef5444bac9a
Gautam Arora <gautamsarora@gmail.com>
2018-09-14 15:25:01 -0400

    + switch cnvault with node-vault

--- src/scripts/startup.env.js
+++ src/scripts/startup.env.js
@@ -1,8 +1,8 @@
-const CNVault = require('./vault')
+const vault = require('./vault')
 const env = process.env.NODE_ENV || 'development'
 
 module.exports = () => {
-  CNVault.then((secrets) => {
+  vault.then((secrets) => {
     let keys = ''
     keys += `export PRIVATE_KEY="${secrets['github-private-key']}"`
     keys += `;export WEBHOOK_PROXY_URL="${secrets['webhook-proxy-url']}"`
