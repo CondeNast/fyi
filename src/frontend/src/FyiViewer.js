f3d139dd0ad9d6b9bcf9a281d6647a74a095d0b5
Gautam Arora <gautamsarora@gmail.com>
2018-10-12 10:22:15 -0400

    + wrap entire page with Visibilty HOC for clarity

--- src/frontend/src/FyiViewer.js
+++ src/frontend/src/FyiViewer.js
@@ -69,137 +69,137 @@ class FyiViewer extends Component {
 
   render() {
     return (
-      <div class='viewer'>
-        <div class='fyi-info col-3'>
-          <PageVisibility onChange={this.handleVisibilityChange}>
-          <Card className="shadow-sm">
-            <CardHeader>About</CardHeader>
-            <CardBody>
-              <CardTitle>{this.state.data.name}</CardTitle>
-              <CardText><Truncate lines={5} dangerouslySetInnerHTML={{ __html: this.state.data.content}} /></CardText>
-              { this.state.data.content ?
-                <div class="btn-toolbar">
-                  <div class="mr-2"><Button outline color="primary" size="sm" onClick={this._handleOnClickViewButton}>View in Confluence</Button></div>
-                  <div><Button outline color="secondary" size="sm" onClick={this._handleOnClickEditButton}>Edit</Button></div>
-                </div> :
-                <Button outline color="primary" size="sm" onClick={this._handleOnClickEditButton}>Write in Confluence</Button>
-              }
-            </CardBody>
-          </Card>
-          </PageVisibility>
-
-          <hr />
-
-          <Card className='shadow-sm fyi-tags' >
-            <CardHeader>Tags</CardHeader>
-              <CardBody className='tag-body'>
-                {this.state.data.tags && this.state.data.tags.filter((t) => !(['drip'].includes(t))).map(function(tag, index){
-                  return <Badge color='info'>
-                    {tag}
-                    <a class='remove-tag-button' href='#' data-tag={tag} onClick={this._handleOnClickDeleteTag.bind(this)}>×</a>
-                    </Badge>
-                }, this)}
+      <PageVisibility onChange={this.handleVisibilityChange}>
+        <div class='viewer'>
+          <div class='fyi-info col-3'>
+            <Card className="shadow-sm">
+              <CardHeader>About</CardHeader>
+              <CardBody>
+                <CardTitle>{this.state.data.name}</CardTitle>
+                <CardText><Truncate lines={5} dangerouslySetInnerHTML={{ __html: this.state.data.content}} /></CardText>
+                { this.state.data.content ?
+                  <div class="btn-toolbar">
+                    <div class="mr-2"><Button outline color="primary" size="sm" onClick={this._handleOnClickViewButton}>View in Confluence</Button></div>
+                    <div><Button outline color="secondary" size="sm" onClick={this._handleOnClickEditButton}>Edit</Button></div>
+                  </div> :
+                  <Button outline color="primary" size="sm" onClick={this._handleOnClickEditButton}>Write in Confluence</Button>
+                }
               </CardBody>
+            </Card>
 
-              <hr data-note='Add Tag' />
+            <hr />
 
-                <CardBody>
-                  <Form>
-                    <Input placeholder="Tag Name" type="text" list="data" onKeyPress={this._handleKeyPressTag.bind(this)} size='sm'/>
-                    <small class='form-text text-muted'>Press enter to add</small>
-                    </Form>
+            <Card className='shadow-sm fyi-tags' >
+              <CardHeader>Tags</CardHeader>
+                <CardBody className='tag-body'>
+                  {this.state.data.tags && this.state.data.tags.filter((t) => !(['drip'].includes(t))).map(function(tag, index){
+                    return <Badge color='info'>
+                      {tag}
+                      <a class='remove-tag-button' href='#' data-tag={tag} onClick={this._handleOnClickDeleteTag.bind(this)}>×</a>
+                      </Badge>
+                  }, this)}
                 </CardBody>
-          </Card>
-        </div>
 
-        <div class='fyi-details col-6 no-gutters'>
-        <datalist id="list-of-fyis">
-          {this.state.fyis.map((fyi, index) =>
-            <option value={fyi.name} key={index}/>
-          )}
-        </datalist>
-        <datalist id="data-orgs">
-        </datalist>
-      	  <div class='fyi-diagram-container shadow-sm' id="treeWrapper">
-        	  { this.state.data.name ? <CenteredTree data={[this.state.data]} /> : <hr/> }
-      	  </div>
+                <hr data-note='Add Tag' />
 
-          <div class='fyi-activity-list'>
-            <h6 className='text-muted'>Latest Activity</h6>
-          { this.state.data.deploys && this.state.data.deploys.found ?
-                (<ListGroup className='shadow-sm'>
-                  {this.state.data.deploys.events.map((repo, index) => {
-                    return Object.keys(repo).map((key) => {
-                      let deployment = repo[key]
-                      if(deployment.fyi.date_happened_human) {
-                        return (
-                          <ListGroupItem color='light'>
-                            <strong>{deployment.fyi.repo_path}:</strong> Deployed to {key.toUpperCase()}<br />
-                            <small class='text-muted'>{deployment.fyi.date_happened_human}</small>
-                          </ListGroupItem>
-                        )
-                      }
-                    })
-                  })}
-                </ListGroup>)
-          :
-            (<div><p className='text-muted'>None found.</p></div>)
-          }
+                  <CardBody>
+                    <Form>
+                      <Input placeholder="Tag Name" type="text" list="data" onKeyPress={this._handleKeyPressTag.bind(this)} size='sm'/>
+                      <small class='form-text text-muted'>Press enter to add</small>
+                      </Form>
+                  </CardBody>
+            </Card>
           </div>
 
-        </div>
+          <div class='fyi-details col-6 no-gutters'>
+          <datalist id="list-of-fyis">
+            {this.state.fyis.map((fyi, index) =>
+              <option value={fyi.name} key={index}/>
+            )}
+          </datalist>
+          <datalist id="data-orgs">
+          </datalist>
+        	  <div class='fyi-diagram-container shadow-sm' id="treeWrapper">
+          	  { this.state.data.name ? <CenteredTree data={[this.state.data]} /> : <hr/> }
+        	  </div>
 
-        <div class='col-8 col-sm-3 fyi-toolpane'>
+            <div class='fyi-activity-list'>
+              <h6 className='text-muted'>Latest Activity</h6>
+            { this.state.data.deploys && this.state.data.deploys.found ?
+                  (<ListGroup className='shadow-sm'>
+                    {this.state.data.deploys.events.map((repo, index) => {
+                      return Object.keys(repo).map((key) => {
+                        let deployment = repo[key]
+                        if(deployment.fyi.date_happened_human) {
+                          return (
+                            <ListGroupItem color='light'>
+                              <strong>{deployment.fyi.repo_path}:</strong> Deployed to {key.toUpperCase()}<br />
+                              <small class='text-muted'>{deployment.fyi.date_happened_human}</small>
+                            </ListGroupItem>
+                          )
+                        }
+                      })
+                    })}
+                  </ListGroup>)
+            :
+              (<div><p className='text-muted'>None found.</p></div>)
+            }
+            </div>
 
-          <Card className='shadow-sm'>
-            <CardHeader>Dependencies</CardHeader>
+          </div>
 
+          <div class='col-8 col-sm-3 fyi-toolpane'>
+
+            <Card className='shadow-sm'>
+              <CardHeader>Dependencies</CardHeader>
+
+                <CardBody className='with-list'>
+                  <Form>
+                    <ul>
+                    {this.state.data.children && this.state.data.children.map((dependent) =>
+                      <li class='fyi-current-dependency'><a href={"/fyis/"+dependent.fyiId + "/" + dependent.name}>{dependent.name}</a> <a data-dep-name={dependent.name} class="remove-dependency-button text-danger" href="#" onClick={this._handleOnClickDeleteDependency.bind(this)}>Disconnect</a></li>
+                    )}
+                    </ul>
+                  </Form>
+                </CardBody>
+                <hr data-note='Add Dependency' />
+                <CardBody>
+                  <Form>
+                    <Input placeholder="Existing FYI Name" type="text" list="list-of-fyis" onKeyPress={this._handleKeyPressDep.bind(this)} size='sm'/>
+                    <small class='form-text text-muted'>Press enter to add</small>
+                  </Form>
+                </CardBody>
+
+            </Card>
+
+            <Card className="shadow-sm">
+              <CardHeader>Related Repositories</CardHeader>
               <CardBody className='with-list'>
-                <Form>
-                  <ul>
-                  {this.state.data.children && this.state.data.children.map((dependent) =>
-                    <li class='fyi-current-dependency'><a href={"/fyis/"+dependent.fyiId + "/" + dependent.name}>{dependent.name}</a> <a data-dep-name={dependent.name} class="remove-dependency-button text-danger" href="#" onClick={this._handleOnClickDeleteDependency.bind(this)}>Disconnect</a></li>
+                <ul>
+                  {this.state.data.repos && this.state.data.repos.map((repo, index) =>
+                    <li class='fyi-current-repo'><a href={`http://github.com/${repo}`}>{repo}</a> <a data-repo-name={repo} class="remove-repo-button text-danger" href="#" onClick={this._handleOnClickDeleteRepository.bind(this)}>Disconnect</a></li>
                   )}
-                  </ul>
-                </Form>
+                </ul>
               </CardBody>
-              <hr data-note='Add Dependency' />
+
+              <hr data-note='Add Repository' />
+
               <CardBody>
                 <Form>
-                  <Input placeholder="Existing FYI Name" type="text" list="list-of-fyis" onKeyPress={this._handleKeyPressDep.bind(this)} size='sm'/>
+                  <Input placeholder="Repo Org" type="select" list="data" onChange={this._handleOnChangeOrg.bind(this)} size='sm' >
+                    {this.state.orgs && this.state.orgs.map((org) =>
+                      <option value={`${org}`}>{org}</option>
+                    )}
+                  </Input>
+                  <small class='form-text text-muted'>Select GitHub organization</small>
+                  <Input placeholder="Repo Name" type="text" onKeyPress={this._handleKeyPressRepo.bind(this)} size='sm'/>
                   <small class='form-text text-muted'>Press enter to add</small>
                 </Form>
               </CardBody>
-
-          </Card>
-
-          <Card className="shadow-sm">
-            <CardHeader>Related Repositories</CardHeader>
-            <CardBody className='with-list'>
-              <ul>
-                {this.state.data.repos && this.state.data.repos.map((repo, index) =>
-                  <li class='fyi-current-repo'><a href={`http://github.com/${repo}`}>{repo}</a> <a data-repo-name={repo} class="remove-repo-button text-danger" href="#" onClick={this._handleOnClickDeleteRepository.bind(this)}>Disconnect</a></li>
-                )}
-              </ul>
-            </CardBody>
-
-            <hr data-note='Add Repository' />
-
-            <CardBody>
-              <Form>
-                <Input placeholder="Repo Org" type="select" list="data" onChange={this._handleOnChangeOrg.bind(this)} size='sm' >
-                  {this.state.orgs && this.state.orgs.map((org) =>
-                    <option value={`${org}`}>{org}</option>
-                  )}
-                </Input>
-                <small class='form-text text-muted'>Select GitHub organization</small>
-                <Input placeholder="Repo Name" type="text" onKeyPress={this._handleKeyPressRepo.bind(this)} size='sm'/>
-                <small class='form-text text-muted'>Press enter to add</small>
-              </Form>
-            </CardBody>
-          </Card>
+            </Card>
+          </div>
         </div>
-      </div>
+      </PageVisibility>
     );
   }
   _handleOnClickDeleteDependency = (event) => {
